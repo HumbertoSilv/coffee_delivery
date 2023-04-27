14655581b3c59581af20fc804174a31a61428970
Humberto <matheushumberto520@gmail.com>
2023-04-26 20:35:11 -0400

    start project, initial setup

---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: 0;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 1px ${(props) => props.theme.yellow} ;
+  }
+
+  html {
+	  font-size: 62.5%;
+  }
+
+  body {
+    background: ${(props) => props.theme.background};
+    color: ${(props) => props.theme["base-text"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input-security, textarea, button {
+    /* font-family: 'Baloo 2', cursive; */
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1.0rem;
+  }
+`
