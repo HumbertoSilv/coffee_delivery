30c1df9fcd38a0b3d4135aabf40a8bad85bda4bc
humberto <matheus.oliveira@sysmap.com.br>
2023-05-06 15:36:12 -0400

    feat: create CoffeeCard component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,46 +1,73 @@
 import {
-  BackgroundHomeContainer,
   HomeContainer,
   InfoContainer,
   ItemsContainer,
+  IntroContainer,
+  IntroBackground,
+  CoffeeListContainer,
+  CoffeeCard,
 } from './styles'
 import Coffee from '../../assets/Coffee.svg'
 import CoffeeIcon from '../../assets/CoffeeIcon.svg'
 import CartIcon from '../../assets/CartIcon.svg'
 import ClockIcon from '../../assets/ClockIcon.svg'
 import PackageIcon from '../../assets/PackageIcon.svg'
+import CoffeeCardImg from '../../assets/Coffee-01.svg'
 
 export const Home = () => {
   return (
-    <BackgroundHomeContainer>
-      <HomeContainer>
-        <InfoContainer>
-          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-          <p>
-            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-            hora
-          </p>
-          <ItemsContainer>
-            <span>
-              <img src={CartIcon} alt="Cart icon" />
-              Compra simples e segura
-            </span>
-            <span>
-              <img src={PackageIcon} alt="Package icon" />
-              Embalagem mantém o café intacto
-            </span>
-            <span>
-              <img src={ClockIcon} alt="Clock icon" />
-              Entrega rápida e rastreada
-            </span>
-            <span>
-              <img src={CoffeeIcon} alt="Coffee icon" />O café chega fresquinho
-              até você
-            </span>
-          </ItemsContainer>
-        </InfoContainer>
-        <img src={Coffee} alt="Coffee" />
-      </HomeContainer>
-    </BackgroundHomeContainer>
+    <HomeContainer>
+      <IntroBackground>
+        <IntroContainer>
+          <InfoContainer>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </p>
+            <ItemsContainer>
+              <span>
+                <img src={CartIcon} alt="Cart icon" />
+                Compra simples e segura
+              </span>
+              <span>
+                <img src={PackageIcon} alt="Package icon" />
+                Embalagem mantém o café intacto
+              </span>
+              <span>
+                <img src={ClockIcon} alt="Clock icon" />
+                Entrega rápida e rastreada
+              </span>
+              <span>
+                <img src={CoffeeIcon} alt="Coffee icon" />O café chega
+                fresquinho até você
+              </span>
+            </ItemsContainer>
+          </InfoContainer>
+          <img src={Coffee} alt="Coffee" />
+        </IntroContainer>
+      </IntroBackground>
+      <CoffeeListContainer>
+        <h1>Nossos cafés</h1>
+        <nav>
+          <CoffeeCard>
+            <img src={CoffeeCardImg} alt="Coffee image" />
+            <span className="tag">TRADICIONAL</span>
+            <h2>Expresso Tradicional</h2>
+            <p>O tradicional café feito com água quente e grãos moídos</p>
+            <footer>
+              R$
+              <span>9,90</span>
+              <div>
+                <button>-</button>
+                <span>0</span>
+                <button>+</button>
+                <div>cart</div>
+              </div>
+            </footer>
+          </CoffeeCard>
+        </nav>
+      </CoffeeListContainer>
+    </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,20 +1,14 @@
 import styled from 'styled-components'
 import Background from '../../assets/Background.svg'
 
-export const BackgroundHomeContainer = styled.div`
-  background-image: url(${Background});
-  width: 100%;
-  padding: 7rem 0;
-`
-
 export const HomeContainer = styled.div`
   display: flex;
-  justify-content: space-between;
-  width: 80%;
-  margin: 40px auto;
+  flex-direction: column;
+  width: 100%;
+  margin: 8.5rem auto;
 `
 export const InfoContainer = styled.div`
-  width: 55%;
+  width: 50%;
 
   h1 {
     font-size: 4.8rem;
@@ -42,5 +36,90 @@ export const ItemsContainer = styled.div`
     gap: 10px;
     font-size: 16px;
     margin-bottom: 1rem;
+  }
+`
+export const IntroBackground = styled.div`
+  background-image: url(${Background});
+  background-size: contain;
+`
+export const IntroContainer = styled.main`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  margin: 5rem auto;
+`
+export const CoffeeListContainer = styled.section`
+  width: 80%;
+  margin: 0 auto;
+
+  h1 {
+    color: red;
+    font-size: 3.4rem;
+    font-family: 'Baloo 2', cursive;
+    font-weight: 800;
+    line-height: 1.2;
+    color: ${(props) => props.theme['base-subtitle']};
+    padding-bottom: 4.5rem;
+  }
+
+  nav {
+    display: flex;
+  }
+`
+export const CoffeeCard = styled.div`
+  background-color: ${(props) => props.theme['base-card']};
+  width: 22%;
+  max-width: 40rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-top-right-radius: 45px;
+  border-bottom-left-radius: 45px;
+  border-top-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  padding: 1rem;
+  margin: 1.5rem;
+
+  img {
+    margin-top: calc(0px - 2.4rem - 6px);
+    width: 50%;
+  }
+
+  .tag {
+    padding: 0.5rem;
+    margin: 1.5rem;
+    background: ${(props) => props.theme['yellow-light']};
+    color: ${(props) => props.theme['yellow-dark']};
+    border-radius: 10px;
+    font-weight: 600;
+  }
+
+  h2 {
+    font-family: 'Baloo 2', cursive;
+    font-size: 2.1rem;
+    padding-bottom: 5px;
+  }
+
+  p {
+    padding-bottom: 40px;
+    text-align: center;
+    font-size: 1.4rem;
+    color: ${(props) => props.theme['base-label']};
+  }
+
+  footer {
+    display: flex;
+    gap: 5px;
+    padding: 0px 20px 10px;
+    font-size: 14px;
+
+    span {
+      /* padding: 0;
+      margin: 0;
+      background: white;
+      color: grey;
+      border-radius: 0px;
+      font-weight: 600; */
+    }
   }
 `
